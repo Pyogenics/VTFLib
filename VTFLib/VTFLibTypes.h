9b967e5338df291648393a44993c405ae505e5ab
Pyogenics <py0g3n1cs@gmail.com>
2023-06-25 16:09:29 +0100

    [LINUX] Consolidated custom types into own header

---
+++ VTFLib/VTFLibTypes.h
@@ -0,0 +1,44 @@
+#ifndef VTFLIBTYPES_H
+#define VTFLIBTYPES_H
+
+#include <cstdint>
+
+// Custom data types
+typedef unsigned char	vlBool;				//!< Boolean value 0/1.
+typedef char			vlChar;				//!< Single signed character.
+typedef unsigned char	vlByte;				//!< Single unsigned byte.
+typedef signed short	vlShort;			//!< Signed short floating point value.
+typedef unsigned short	vlUShort;			//!< Unsigned short floating point value.
+typedef signed int		vlInt;				//!< Signed integer value.
+typedef unsigned int	vlUInt;				//!< Unsigned integer value.
+typedef signed long		vlLong;				//!< Signed long number.
+typedef unsigned long	vlULong;			//!< Unsigned long number.
+typedef float			vlSingle;			//!< Floating point number
+typedef double			vlDouble;			//!< Double number
+typedef void			vlVoid;				//!< Void value.
+
+typedef uint8_t		vlUInt8;
+typedef uint16_t	vlUInt16;
+typedef uint32_t	vlUInt32;
+typedef uint64_t	vlUInt64;
+
+typedef vlSingle		vlFloat;			//!< Floating point number (same as vlSingled).
+
+#define vlFalse 0
+#define vlTrue 1
+
+#ifdef LINUX
+typedef void *HANDLE; // Pyogenics: Glad I use linux.
+#endif
+
+#ifdef WINDOWS
+	#ifdef VTFLIB_EXPORTS
+	#define VTFLIB_API __declspec(dllexport)
+	#else
+	#define VTFLIB_API __declspec(dllimport)
+	#endif
+#else
+	#define VTFLIB_API __attribute__((visibility("default"))) //XXX: Stupid compiler specific code (GCC)
+#endif
+
+#endif //TYPES_H
--- VTFLib/stdafx.h
+++ VTFLib/stdafx.h
@@ -26,30 +26,6 @@
 #	define VTFLIB_API __declspec(dllimport)
 #endif
 
-// Custom data types
-typedef unsigned char	vlBool;				//!< Boolean value 0/1.
-typedef char			vlChar;				//!< Single signed character.
-typedef unsigned char	vlByte;				//!< Single unsigned byte.
-typedef signed short	vlShort;			//!< Signed short floating point value.
-typedef unsigned short	vlUShort;			//!< Unsigned short floating point value.
-typedef signed int		vlInt;				//!< Signed integer value.
-typedef unsigned int	vlUInt;				//!< Unsigned integer value.
-typedef signed long		vlLong;				//!< Signed long number.
-typedef unsigned long	vlULong;			//!< Unsigned long number.
-typedef float			vlSingle;			//!< Floating point number
-typedef double			vlDouble;			//!< Double number
-typedef void			vlVoid;				//!< Void value.
-
-typedef unsigned __int8		vlUInt8;
-typedef unsigned __int16	vlUInt16;
-typedef unsigned __int32	vlUInt32;
-typedef unsigned __int64	vlUInt64;
-
-typedef vlSingle		vlFloat;			//!< Floating point number (same as vlSingled).
-
-#define vlFalse			0
-#define vlTrue			1
-
 #if _MSC_VER >= 1400
 #	define _CRT_SECURE_NO_WARNINGS
 #	define _CRT_NONSTDC_NO_DEPRECATE
